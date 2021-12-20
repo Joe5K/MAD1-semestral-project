3c7d42e7a1f52fec61aa0e9f4383bc03087a7488
personal <jozo.krkosk4@gmail.com>
2021-12-20 15:23:42 +0100

    added floyd

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
+import math
+from copy import deepcopy
+
 from numpy import random
 
-NUMBER_OF_NODES = 1000
-M = 3
+NUMBER_OF_NODES = 300
+M = 2
 
 class Node:
     def __init__(self, number):
@@ -17,6 +20,8 @@ class BAGraph:
         self.edges = []
         self.nodes = {}
         self.current_number = self.m = m
+        self.matrix = None
+        self.floyd = None
 
     def get_node(self, number):
         if not self.nodes.get(number):
@@ -24,7 +29,7 @@ class BAGraph:
         return self.nodes[number]
 
     def generate_initial_graph(self):
-        for i in range(self.m-1, 0, -1):
+        for i in range(self.m, 0, -1):
             for j in range(i):
                 self.edges.append((self.get_node(i), self.get_node(j)))
                 self.get_node(i).weight += 1
@@ -54,10 +59,41 @@ class BAGraph:
             self.get_node(i).weight += 1
         new_node.weight = self.m
 
+    def to_csv(self):
+        with open(f"n={NUMBER_OF_NODES}_m={M}.csv", "w") as ass:
+            ass.write("\n".join([*["Source;Target"], *[f"{i[0].number};{i[1].number}" for i in self.edges]])+"\n")
+
+    def to_matrix(self):
+        data = []
+        for (i, j) in self.edges:
+            data.append((i.number, j.number))
+        matrix = [[math.inf for _ in range(NUMBER_OF_NODES)] for __ in range(NUMBER_OF_NODES)]
+
+        for (i, j) in data:
+            matrix[i][j] = 1
+            matrix[j][i] = 1
+        self.matrix = matrix
+
+    def to_floyd(self):
+        if not self.matrix:
+            self.to_matrix()
+        V = len(self.matrix)
+        dist = deepcopy(self.matrix)
+
+        for k in range(V):
+            print(k)
+            for i in range(V):
+                for j in range(V):
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+
+        self.floyd = dist
 
 graph = BAGraph(M)
 graph.generate_initial_graph()
 
-for i in range(NUMBER_OF_NODES-M):
+for i in range(NUMBER_OF_NODES-M-1):
     graph.add_node()
-print(graph)
+
+
+graph.to_floyd()
+pass
